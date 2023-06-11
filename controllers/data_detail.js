4b44a480ecabeaac07b9ae2f9f52caafaba334cf
Yongyuth Chaipa <nas.coke@gmail.com>
2023-06-11 21:48:25 +0700

    edit file

--- controllers/data_detail.js
+++ controllers/data_detail.js
@@ -1,143 +1,168 @@
 const DataDetailModel = require("../models/data_detail")
 
 exports.addDataDetail = async (req, res, next) => {
-    console.log(req.body)
-    const addHoliday = new DataDetailModel({
-        peacode:req.body.peacode,
-        office_name:req.body.office_name,
-        ca_no:req.body.ca_no,
-        name_customer:req.body.name_customer,
-        rate:req.body.rate,
-        pea_no:req.body.pea_no,
-        billing_Cycle:req.body.billing_Cycle,
-        year:req.body.year,
-        cause:req.body.cause,
-        in_cause:req.body.in_cause,
-        data_broken:{
-            normal_date_start:req.body.normal_date_start,
-            normal_date_end:req.body.normal_date_end,
-            brok_date_start:req.body.brok_date_start,
-            brok_date_end:req.body.brok_date_end,
-            p_normal:req.body.p_normal,
-            op_normal:req.body.op_normal,
-            h_normal:req.body.h_normal,
-            sum_normal:req.body.sum_normal,
-            p_brok:req.body.p_brok,
-            op_brok:req.body.op_brok,
-            h_brok:req.body.h_brok,
-            sum_brok:req.body.sum_brok,
-            sum_all:req.body.sum_all
-        },
-        unit_use:{
-            kwh_p:{
-                unit_read_after_kwh_p:req.body.unit_read_after_kwh_p,
-                unit_read_before_kwh_p:req.body.unit_read_before_kwh_p,
-                multiplier_kwh_p:req.body.multiplier_kwh_p,
-                unit_normal_kwh_p:req.body.unit_normal_kwh_p,
-                date_normal_kwh_p:req.body.date_normal_kwh_p,
-                avg_date_kwh_p:req.body.avg_date_kwh_p,
-                date_brok_kwh_p:req.body.date_brok_kwh_p,
-                unit_avg_to_sap_kwh_p:req.body.unit_avg_to_sap_kwh_p
-            },
-            kwh_op:{
-                unit_read_after_kwh_op:req.body.unit_read_after_kwh_op,
-                unit_read_before_kwh_op:req.body.unit_read_before_kwh_op,
-                multiplier_kwh_op:req.body.multiplier_kwh_op,
-                unit_normal_kwh_op:req.body.unit_normal_kwh_op,
-                date_normal_kwh_op:req.body.date_normal_kwh_op,
-                avg_date_kwh_op:req.body.avg_date_kwh_op,
-                date_brok_kwh_op:req.body.date_brok_kwh_op,
-                unit_avg_to_sap_kwh_op:req.body.unit_avg_to_sap_kwh_op
-            },
-            kwh_h:{
-                unit_read_after_kwh_h:req.body.unit_read_after_kwh_h,
-                unit_read_before_kwh_h:req.body.unit_read_before_kwh_h,
-                multiplier_kwh_h:req.body.multiplier_kwh_h,
-                unit_normal_kwh_h:req.body.unit_normal_kwh_h,
-                date_normal_kwh_h:req.body.date_normal_kwh_h,
-                avg_date_kwh_h:req.body.avg_date_kwh_h,
-                date_brok_kwh_h:req.body.date_brok_kwh_h,
-                unit_avg_to_sap_kwh_h:req.body.unit_avg_to_sap_kwh_h
-            },
-        },
-        kwh_unit:{
-            kwh_p:{
-                unit_read_after_unit_kwh_p:req.body.unit_read_after_unit_kwh_p,
-                unit_read_before_unit_kwh_p:req.body.unit_read_before_unit_kwh_p,
-                multiplier_unit_kwh_p:req.body.multiplier_unit_kwh_p,
-                kwh_normal_unit_kwh_p:req.body.kwh_normal_unit_kwh_p      
-             },
-             kwh_op:{
-                unit_read_after_unit_kwh_op:req.body.unit_read_after_unit_kwh_op,
-                unit_read_before_unit_kwh_op:req.body.unit_read_before_unit_kwh_op,
-                multiplier_unit_kwh_op:req.body.multiplier_unit_kwh_op,
-                kwh_normal_unit_kwh_op:req.body.kwh_normal_unit_kwh_op       
-             },
-             kwh_h:{
-                unit_read_after_unit_kwh_h:req.body.unit_read_after_unit_kwh_h,
-                unit_read_before_unit_kwh_h:req.body.unit_read_before_unit_kwh_h,
-                multiplier_unit_kwh_h:req.body.multiplier_unit_kwh_h,
-                kwh_normal_unit_kwh_h:req.body.kwh_normal_unit_kwh_h    
-             },
-             
-        },
-        user:req.body.user
+  //console.log(req.body)
+  const addHoliday = new DataDetailModel({
+    peacode: req.body.peacode,
+    office_name: req.body.office_name,
+    ca_no: req.body.ca_no,
+    name_customer: req.body.name_customer,
+    rate: req.body.rate,
+    pea_no: req.body.pea_no,
+    type_customer: req.body.type_customer,
+    address: req.body.address,
+    billing_Cycle: req.body.billing_Cycle,
+    year: req.body.year,
+    cause: req.body.cause,
+    in_cause: req.body.in_cause,
+    data_broken: {
+      normal_date_start: req.body.normal_date_start,
+      normal_date_end: req.body.normal_date_end,
+      brok_date_start: req.body.brok_date_start,
+      brok_date_end: req.body.brok_date_end,
+      p_normal: req.body.p_normal,
+      op_normal: req.body.op_normal,
+      h_normal: req.body.h_normal,
+      sum_normal: req.body.sum_normal,
+      p_brok: req.body.p_brok,
+      op_brok: req.body.op_brok,
+      h_brok: req.body.h_brok,
+      sum_brok: req.body.sum_brok,
+      sum_all: req.body.sum_all
+    },
+    unit_use: {
+      kwh_p: {
+        unit_read_after_kwh_p: req.body.unit_read_after_kwh_p,
+        unit_read_before_kwh_p: req.body.unit_read_before_kwh_p,
+        multiplier_kwh_p: req.body.multiplier_kwh_p,
+        unit_normal_kwh_p: req.body.unit_normal_kwh_p,
+        date_normal_kwh_p: req.body.date_normal_kwh_p,
+        avg_date_kwh_p: req.body.avg_date_kwh_p,
+        date_brok_kwh_p: req.body.date_brok_kwh_p,
+        unit_avg_to_sap_kwh_p: req.body.unit_avg_to_sap_kwh_p
+      },
+      kwh_op: {
+        unit_read_after_kwh_op: req.body.unit_read_after_kwh_op,
+        unit_read_before_kwh_op: req.body.unit_read_before_kwh_op,
+        multiplier_kwh_op: req.body.multiplier_kwh_op,
+        unit_normal_kwh_op: req.body.unit_normal_kwh_op,
+        date_normal_kwh_op: req.body.date_normal_kwh_op,
+        avg_date_kwh_op: req.body.avg_date_kwh_op,
+        date_brok_kwh_op: req.body.date_brok_kwh_op,
+        unit_avg_to_sap_kwh_op: req.body.unit_avg_to_sap_kwh_op
+      },
+      kwh_h: {
+        unit_read_after_kwh_h: req.body.unit_read_after_kwh_h,
+        unit_read_before_kwh_h: req.body.unit_read_before_kwh_h,
+        multiplier_kwh_h: req.body.multiplier_kwh_h,
+        unit_normal_kwh_h: req.body.unit_normal_kwh_h,
+        date_normal_kwh_h: req.body.date_normal_kwh_h,
+        avg_date_kwh_h: req.body.avg_date_kwh_h,
+        date_brok_kwh_h: req.body.date_brok_kwh_h,
+        unit_avg_to_sap_kwh_h: req.body.unit_avg_to_sap_kwh_h
+      },
+    },
+    kwh_unit: {
+      kwh_p: {
+        unit_read_after_unit_kwh_p: req.body.unit_read_after_unit_kwh_p,
+        unit_read_before_unit_kwh_p: req.body.unit_read_before_unit_kwh_p,
+        multiplier_unit_kwh_p: req.body.multiplier_unit_kwh_p,
+        kwh_normal_unit_kwh_p: req.body.kwh_normal_unit_kwh_p
+      },
+      kwh_op: {
+        unit_read_after_unit_kwh_op: req.body.unit_read_after_unit_kwh_op,
+        unit_read_before_unit_kwh_op: req.body.unit_read_before_unit_kwh_op,
+        multiplier_unit_kwh_op: req.body.multiplier_unit_kwh_op,
+        kwh_normal_unit_kwh_op: req.body.kwh_normal_unit_kwh_op
+      },
+      kwh_h: {
+        unit_read_after_unit_kwh_h: req.body.unit_read_after_unit_kwh_h,
+        unit_read_before_unit_kwh_h: req.body.unit_read_before_unit_kwh_h,
+        multiplier_unit_kwh_h: req.body.multiplier_unit_kwh_h,
+        kwh_normal_unit_kwh_h: req.body.kwh_normal_unit_kwh_h
+      },
+
+    },
+    user: req.body.user,
+    comment: req.body.comment,
+  });
+
+  try {
+    const result = await addHoliday.save();
+    res.status(200).json({
+      message: "success",
+      node: result,
     });
-  
-    try {
-      const result = await addHoliday.save();
-      res.status(200).json({
-        message: "success",
-        node: result,
-      });
-    } catch (err) {
-      res.json({
-        message: err,
-      });
-    }
-  };
+  } catch (err) {
+    res.json({
+      message: err,
+    });
+  }
+};
 
-  exports.getListByPeaCode = async (req, res, next) => {
-    console.log(req.query.peacode)
-    const peacode = req.query.peacode;
-  
-    try {
-      const getListByPeaCode = await DataDetailModel.find({
-        peacode: peacode,
-      });
-      // res.status(200).json({
-      //   //   message: "success",
-      //   //   data: getHoliday,
-  
-      //   getHoliday,
-      // });
-      res.send(getListByPeaCode);
-    } catch (err) {
-      res.json({
-        message: err,
-      });
-    }
-  };
+exports.getListByPeaCode = async (req, res, next) => {
+  console.log(req.query.peacode)
+  const peacode = req.query.peacode;
 
-  exports.getById = async (req, res, next) => {
-    const id = req.query.id;
-    console.log(req.query)
-  
-    try {
-      const getById = await DataDetailModel.find({
-        _id: id,
-      });
-      // res.status(200).json({
-      //   //   message: "success",
-      //   //   data: getHoliday,
-  
+  try {
+    const getListByPeaCode = await DataDetailModel.find({
+      peacode: peacode,
+    });
+    // res.status(200).json({
+    //   //   message: "success",
+    //   //   data: getHoliday,
+
+    //   getHoliday,
+    // });
+    res.send(getListByPeaCode);
+  } catch (err) {
+    res.json({
+      message: err,
+    });
+  }
+};
+
+exports.updateById = async (req, res, next) => {
+  const id = req.query.id;
+  //console.log(req.query)
+
+  try {
+    const updateById = await DataDetailModel.findOneAndUpdate(
+      { _id: _id },
+      { $set: { dateHoliday: dateHoliday, nameHoliday: nameHoliday } }
+    );
+    res.status(200).json({
+      message: "success",
+      data: updateById,
       //   getHoliday,
-      // });
-      res.send(getById);
-      console.log(getById)
-    } catch (err) {
-      res.json({
-        message: err,
-      });
-    }
-  };
+    });
+    // res.send(updateHoliday);
+  } catch (err) {
+    res.json({
+      message: err,
+    });
+  }
+};
+
+exports.getById = async (req, res, next) => {
+  const id = req.query.id;
+  // console.log(req.query)
+
+  try {
+    const getById = await DataDetailModel.find({
+      _id: id,
+    });
+    // res.status(200).json({
+    //   //   message: "success",
+    //   //   data: getHoliday,
+
+    //   getHoliday,
+    // });
+    res.send(getById);
+    //console.log(getById)
+  } catch (err) {
+    res.json({
+      message: err,
+    });
+  }
+};
--- models/data_detail.js
+++ models/data_detail.js
@@ -1,210 +1,219 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema;
 
-const dataDetail  =  new Schema(
+const dataDetail = new Schema(
     {
-        peacode:{
-            type:String
+        peacode: {
+            type: String
         },
-        office_name:{
-            type:String
+        office_name: {
+            type: String
         },
-        ca_no:{
-            type:String
+        ca_no: {
+            type: String
         },
-        name_customer:{
-            type:String
+        name_customer: {
+            type: String
         },
-        rate:{
-            type:String
+        rate: {
+            type: String
         },
-        pea_no:{
-            type:String
+        pea_no: {
+            type: String
         },
-        billing_Cycle:{
-            type:String
+        type_customer: {
+            type: String
         },
-        year:{
-            type:String
+        address: {
+            type: String
         },
-        cause:{
-            type:String
+        billing_Cycle: {
+            type: String
         },
-        in_cause:{
-            type:String
+        year: {
+            type: String
         },
-        data_broken:{
-            normal_date_start:{
-                type:String
+        cause: {
+            type: String
+        },
+        in_cause: {
+            type: String
+        },
+        data_broken: {
+            normal_date_start: {
+                type: String
             },
-            normal_date_end:{
-                type:String
+            normal_date_end: {
+                type: String
             },
-            brok_date_start:{
-                type:String
+            brok_date_start: {
+                type: String
             },
-            brok_date_end:{
-                type:String
+            brok_date_end: {
+                type: String
             },
-            p_normal:{
-                type:String
+            p_normal: {
+                type: String
             },
-            op_normal:{
-                type:String
+            op_normal: {
+                type: String
             },
-            h_normal:{
-                type:String
+            h_normal: {
+                type: String
             },
-            sum_normal:{
-                type:String
+            sum_normal: {
+                type: String
             },
-            p_brok:{
-                type:String
+            p_brok: {
+                type: String
             },
-            op_brok:{
-                type:String
+            op_brok: {
+                type: String
             },
-            h_brok:{
-                type:String
+            h_brok: {
+                type: String
             },
-            sum_brok:{
-                type:String
+            sum_brok: {
+                type: String
             },
-            sum_all:{
-                type:String
+            sum_all: {
+                type: String
             }
         },
-        unit_use:{
-            kwh_p:{
-                unit_read_after_kwh_p:{
-                    type:String
+        unit_use: {
+            kwh_p: {
+                unit_read_after_kwh_p: {
+                    type: String
                 },
-                unit_read_before_kwh_p:{
-                    type:String
+                unit_read_before_kwh_p: {
+                    type: String
                 },
-                multiplier_kwh_p:{
-                    type:String
+                multiplier_kwh_p: {
+                    type: String
                 },
-                unit_normal_kwh_p:{
-                    type:String
+                unit_normal_kwh_p: {
+                    type: String
                 },
-                date_normal_kwh_p:{
-                    type:String
+                date_normal_kwh_p: {
+                    type: String
                 },
-                avg_date_kwh_p:{
-                    type:String
+                avg_date_kwh_p: {
+                    type: String
                 },
-                date_brok_kwh_p:{
-                    type:String
+                date_brok_kwh_p: {
+                    type: String
                 },
-                unit_avg_to_sap_kwh_p:{
-                    type:String
+                unit_avg_to_sap_kwh_p: {
+                    type: String
                 }
             },
-            kwh_op:{
-                unit_read_after_kwh_op:{
-                    type:String
+            kwh_op: {
+                unit_read_after_kwh_op: {
+                    type: String
                 },
-                unit_read_before_kwh_op:{
-                    type:String
+                unit_read_before_kwh_op: {
+                    type: String
                 },
-                multiplier_kwh_op:{
-                    type:String
+                multiplier_kwh_op: {
+                    type: String
                 },
-                unit_normal_kwh_op:{
-                    type:String
+                unit_normal_kwh_op: {
+                    type: String
                 },
-                date_normal_kwh_op:{
-                    type:String
+                date_normal_kwh_op: {
+                    type: String
                 },
-                avg_date_kwh_op:{
-                    type:String
+                avg_date_kwh_op: {
+                    type: String
                 },
-                date_brok_kwh_op:{
-                    type:String
+                date_brok_kwh_op: {
+                    type: String
                 },
-                unit_avg_to_sap_kwh_op:{
-                    type:String
+                unit_avg_to_sap_kwh_op: {
+                    type: String
                 }
             },
-            kwh_h:{
-                unit_read_after_kwh_h:{
-                    type:String
+            kwh_h: {
+                unit_read_after_kwh_h: {
+                    type: String
                 },
-                unit_read_before_kwh_h:{
-                    type:String
+                unit_read_before_kwh_h: {
+                    type: String
                 },
-                multiplier_kwh_h:{
-                    type:String
+                multiplier_kwh_h: {
+                    type: String
                 },
-                unit_normal_kwh_h:{
-                    type:String
+                unit_normal_kwh_h: {
+                    type: String
                 },
-                date_normal_kwh_h:{
-                    type:String
+                date_normal_kwh_h: {
+                    type: String
                 },
-                avg_date_kwh_h:{
-                    type:String
+                avg_date_kwh_h: {
+                    type: String
                 },
-                date_brok_kwh_h:{
-                    type:String
+                date_brok_kwh_h: {
+                    type: String
                 },
-                unit_avg_to_sap_kwh_h:{
-                    type:String
+                unit_avg_to_sap_kwh_h: {
+                    type: String
                 }
             },
         },
-        kwh_unit:{
-            kwh_p:{
-                unit_read_after_unit_kwh_p:{
-                    type:String
+        kwh_unit: {
+            kwh_p: {
+                unit_read_after_unit_kwh_p: {
+                    type: String
                 },
-                unit_read_before_unit_kwh_p:{
-                    type:String
+                unit_read_before_unit_kwh_p: {
+                    type: String
                 },
-                multiplier_unit_kwh_p:{
-                    type:String
+                multiplier_unit_kwh_p: {
+                    type: String
                 },
-                kwh_normal_unit_kwh_p:{
-                    type:String
+                kwh_normal_unit_kwh_p: {
+                    type: String
                 }
             },
-            kwh_op:{
-                unit_read_after_unit_kwh_op:{
-                    type:String
+            kwh_op: {
+                unit_read_after_unit_kwh_op: {
+                    type: String
                 },
-                unit_read_before_unit_kwh_op:{
-                    type:String
+                unit_read_before_unit_kwh_op: {
+                    type: String
                 },
-                multiplier_unit_kwh_op:{
-                    type:String
+                multiplier_unit_kwh_op: {
+                    type: String
                 },
-                kwh_normal_unit_kwh_op:{
-                    type:String
+                kwh_normal_unit_kwh_op: {
+                    type: String
                 }
             },
-            kwh_h:{
-                unit_read_after_unit_kwh_h:{
-                    type:String
+            kwh_h: {
+                unit_read_after_unit_kwh_h: {
+                    type: String
                 },
-                unit_read_before_unit_kwh_h:{
-                    type:String
+                unit_read_before_unit_kwh_h: {
+                    type: String
                 },
-                multiplier_unit_kwh_h:{
-                    type:String
+                multiplier_unit_kwh_h: {
+                    type: String
                 },
-                kwh_normal_unit_kwh_h:{
-                    type:String
+                kwh_normal_unit_kwh_h: {
+                    type: String
                 }
             }
         },
-        user:{
-            type:String
+        user: {
+            type: String
         },
-        createAt:{
+        comment: {
+            type: String
+        },
+        createAt: {
             type: Date, default: Date.now
         }
     }
 )
 
-module.exports = mongoose.model("DataDetail",dataDetail);
+module.exports = mongoose.model("DataDetail", dataDetail);
