7ee89f52bc5dbdf4599b868a269c60b9016f718b
nongsoi10 <nongsoi10007@gmail.com>
2023-06-18 23:45:28 +0700

    add get data form index

--- routes/data_detail_router.js
+++ routes/data_detail_router.js
@@ -1,14 +1,15 @@
-const express = require("express")
+const express = require("express");
 
-const DataDetail = require("../controllers/data_detail")
+const DataDetail = require("../controllers/data_detail");
 
 const routerData = express.Router();
 
-routerData.post("/add", DataDetail.addDataDetail)
-routerData.get("/databypeacode", DataDetail.getListByPeaCode)
-routerData.get("/id", DataDetail.getById)
-routerData.delete("/delete", DataDetail.DeleteId)
-routerData.put('/update', DataDetail.updateById)
-routerData.get('/all', DataDetail.getAll)
+routerData.post("/add", DataDetail.addDataDetail);
+routerData.get("/databypeacode", DataDetail.getListByPeaCode);
+routerData.get("/id", DataDetail.getById);
+routerData.delete("/delete", DataDetail.DeleteId);
+routerData.put("/update", DataDetail.updateById);
+routerData.post("/all", DataDetail.getAll);
+routerData.post("/count", DataDetail.countDetail);
 
 module.exports = routerData;
